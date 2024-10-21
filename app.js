9202f6be51b89c86d526316718c08e7090546543
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-21 16:15:15 +0900

    app.js

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-require(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //res.body가 객체로 인식 된다.
 
-const mongoURI = process.env.LOCAL_DB_ACCRESS;
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
 //mongoose　셋팅
 
 mongoose
     .connect(mongoURI, { useNewUrlParser: true })
     .then(() => console.log('mongoose connected'))
     .catch((err) => console.log('DB connected error', err));
+//port
+app.listen(process.env.PORT || 8080, () => {
+    console.log('server on');
+});
